ae13b1cdc8dd3e39265ab5b7617599d5582a2353
Nikko Adrian <nikkoadrian02@gmail.com>
2022-06-14 10:41:17 +0700

    edit c

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -3,7 +3,7 @@
 <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img class="w-100" src="{{ asset('assets/website/img/carousel-1.jpg') }}" alt="Image">
+            <img class="w-100" src="{{ asset('assets/website/img/carousel-1.JPG') }}" alt="Image">
             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                 <div class="p-3" style="max-width: 900px;">
                     <h5 class="text-white text-uppercase mb-3 animated slideInDown">Sekolah Pusat Keunggulan</h5>
@@ -12,7 +12,7 @@
             </div>
         </div>
         <div class="carousel-item">
-            <img class="w-100" src="{{ asset('assets/website/img/carousel-2.jpg') }}" alt="Image">
+            <img class="w-100" src="{{ asset('assets/website/img/carousel-2.JPG') }}" alt="Image">
             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                 <div class="p-3" style="max-width: 900px;">
                     <h5 class="text-white text-uppercase mb-3 animated slideInDown">Sekolah Pusat Keunggulan</h5>
